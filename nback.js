b073fd60b62e87c93f30720e2597efedd5abf545
Elliot Dauber <Elliot@Elliots-MacBook-Pro.local>
2023-08-06 20:51:34 -0700

    new visual mode and per-game stats

--- nback.js
+++ nback.js
@@ -4,32 +4,13 @@ class NbackGame {
     this.settings = settings;
     this.currentRound = -1;
     this.correctAnswers = 0;
+    this.gameStats = new GameStats();
   }
 
-  generateItem() {
-    if (this.sequence.length >= this.settings.nback && Math.random() < 0.45) {
-      return this.sequence[this.sequence.length - this.settings.nback];
-    }
-    var index = Math.floor(Math.random() * this.settings.letters.length);
-    var item = this.settings.letters[index];
-    return item;
-  }
-
-  displayItem(item) {
-    if (this.settings.auditory) {
-      var msg = new SpeechSynthesisUtterance();
-      msg.text = item.toLowerCase();
-      window.speechSynthesis.speak(msg);
-    }
-    if (this.settings.visual) {
-      $("#display").html("");
-      $("#display").html(item);
-    }
-  }
+  onStart() {}
 
   startGame() {
     $("#game-display").show();
-    $("#display").html("");
     $("#score").html(0);
     $("#score-board").css("color", "black");
     $("#yes").hide();
@@ -37,6 +18,7 @@ class NbackGame {
     $("#next").show();
     $("#correct").hide();
     $("#incorrect").hide();
+    this.onStart();
     for (var i = 0; i < this.settings.numItems; i++) {
       this.sequence.push(this.generateItem());
     }
@@ -44,7 +26,6 @@ class NbackGame {
   }
 
   nextRound() {
-    $("#display").html("");
     this.currentRound++;
     this.currentItem = this.sequence[this.currentRound];
     this.displayItem(this.currentItem);
@@ -74,9 +55,11 @@ class NbackGame {
   updateScore(correct) {
     $("#score").html(this.correctAnswers);
     if (correct) {
+      this.gameStats.increment_correct();
       $("#correct").show();
       $("#incorrect").hide();
     } else {
+      this.gameStats.increment_incorrect();
       $("#incorrect").show();
       $("#correct").hide();
     }
@@ -93,11 +76,58 @@ class NbackGame {
       stats.set_winning_streak(0);
       stats.set_losses(stats.losses + 1);
     }
+    console.log("ending game, adding stats");
+    stats.add_game_stats(this.gameStats);
 
     alert("Game over! You scored " + this.correctAnswers + " out of " + this.settings.numItems);
     if (stats.winning_streak >= 3) {
       alert("You have completed 3 rounds at 100% accuracy! We suggest going to the next nback level. Go to the settings tab to do this. Great work!")
     }
+  }
+}
+
+class AuditoryNbackGame extends NbackGame {
+  generateItem() {
+    if (this.sequence.length >= this.settings.nback && Math.random() < 0.45) {
+      return this.sequence[this.sequence.length - this.settings.nback];
+    }
+    var index = Math.floor(Math.random() * this.settings.letters.length);
+    var item = this.settings.letters[index];
+    return item;
+  }
+
+  displayItem(item) {
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = item.toLowerCase();
+    window.speechSynthesis.speak(msg);
+  }
+
+  onStart() {
+    $("#display").hide();
+  }
+}
+
+class VisualNbackGame extends NbackGame {
+  generateItem() {
+    if (this.sequence.length >= this.settings.nback && Math.random() < 0.45) {
+      return this.sequence[this.sequence.length - this.settings.nback];
+    }
+    return Math.floor(Math.random() * 9);
+  }
+
+  displayItem(item) {
+    const displayBoxList = $('.display-box').toArray();
+    displayBoxList.forEach((displayBox, index) => {
+      displayBox.classList.remove("active");
+      if (index == item) {
+        displayBox.classList.add("active");
+      }
+    })
+  }
+
+  onStart() {
+    this.displayItem(-1);
+    $("#display").show();
   }
 }
 
@@ -122,8 +152,24 @@ class Settings {
   }
 }
 
+class GameStats {
+  constructor() {
+    this.correct = 0;
+    this.incorrect = 0;
+  }
+
+  increment_correct() {
+    this.correct++;
+  }
+
+  increment_incorrect() {
+    this.incorrect++;
+  }
+}
+
 class Stats {
   constructor() {
+    this.gameStats = [];
     this.set_wins(0);
     this.set_losses(0);
     this.set_winning_streak(0);
@@ -155,18 +201,32 @@ class Stats {
     this.num_wrong = num_wrong;
     $("#stats-num-wrong").html(num_wrong);
   }
+
+  add_game_stats(gameStats) {
+    this.gameStats.push(gameStats);
+    console.log("game stats: " + this.gameStats.length);
+  }
 }
 
 //should probably be singletons
 var stats = new Stats()
-var settings = new Settings(2, 10, $("#self-paced").prop("checked"));
+var settings = new Settings(2, 20, $("#self-paced").prop("checked"));
 
 var game = null;
+
+
+// EVENT HANDLERS
+
 //create a new game and start it when the start-game button is clicked
 $("#start-game").click(function() {
   $("#stats-display").hide();
   $("#settings-display").hide();
-  game = new NbackGame(settings);
+  // TODO: change mode to enum
+  if (settings.visual) {
+    game = new VisualNbackGame(settings);
+  } else if (settings.auditory) {
+    game = new AuditoryNbackGame(settings);
+  }
   game.startGame();
 });
 
@@ -226,7 +286,31 @@ $("#goto-settings").click(() => {
 $("#goto-stats").click(() => {
   $("#game-display").hide();
   $("#settings-display").hide();
-  $("#stats-display").show();
+
+  const tableBody = $("#pergame-stats-table tbody");
+  // Clear existing rows
+  tableBody.empty();
+  // Generate rows for each game
+
+  console.log("game stats num: " + stats.gameStats.length);
+  stats.gameStats.forEach(game => {
+    const accuracy = ((game.correct / (game.correct + game.incorrect)) * 100).toFixed(2);
+
+    const row = $("<tr>");
+    console.log(row);
+    row.html(`
+      <td>${game.correct}</td>
+      <td>${game.incorrect}</td>
+      <td>${accuracy}%</td>
+    `);
+    if (accuracy == 100.00) {
+      row.addClass("flawless");
+    }
+
+    tableBody.append(row);
+  });
+
+  $("#stats-display").css('display', 'flex');
 });
 
 $("#save-settings").click(() => {
@@ -239,3 +323,33 @@ $("#save-settings").click(() => {
   settings.selfPaced = $("#self-paced").prop("checked");
   alert("Settings saved!");
 });
+
+$("#print-stats").click(print_stats);
+
+
+// PRINTING
+
+function print_stats() {
+  const printContents = document.getElementById("stats-display").outerHTML;
+  const popupWindow = window.open("", "_blank", "width=600, height=600");
+  popupWindow.document.open();
+  popupWindow.document.write(`
+    <html>
+    <head>
+      <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+      ${printContents}
+      <script type="text/javascript">
+        window.onload = function() {
+          window.print();
+          window.onafterprint = function() {
+            window.close();
+          };
+        };
+      </script>
+    </body>
+    </html>
+  `);
+  popupWindow.document.close();
+}
